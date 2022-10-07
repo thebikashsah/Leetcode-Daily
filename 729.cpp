60e668e41011dff5a883b22034d3bcd4e441b64f
Bikash Sah <sahbikash2211@gmail.com>
2022-10-07 19:34:18 +0530

    Day 7 Added

---
+++ 729.cpp
@@ -0,0 +1,22 @@
+class MyCalendar {
+public:
+    set< pair<int,int > > s;
+    MyCalendar() {
+        
+    }
+    
+    bool book(int start, int end) {
+        auto it=s.upper_bound({start,end});
+        
+        if(it!=s.end() && end>it->second)
+                return false;
+        s.insert({end,start});
+            return true;
+    }
+};
+
+/**
+ * Your MyCalendar object will be instantiated and called as such:
+ * MyCalendar* obj = new MyCalendar();
+ * bool param_1 = obj->book(start,end);
+ */
