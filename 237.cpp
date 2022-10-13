bf3660e89504e02d918ea65120978984f2949f29
Bikash Sah <sahbikash2211@gmail.com>
2022-10-13 08:13:49 +0530

    Day 13 Added

---
+++ 237.cpp
@@ -0,0 +1,18 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    void deleteNode(ListNode* node) {
+            ListNode* t=node->next;
+            node->val=t->val;
+            node->next=t->next;
+            delete(t);
+        
+    }
+};
